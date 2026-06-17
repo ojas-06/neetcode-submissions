9f40bd83e2bfcab452d267adc7619ed7ec26efce
Ojas Deshpande <ojas.md2006@gmail.com>
2026-06-17 15:24:00 +0530

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1) return nums[0];
+        vector<int> dp(n);
+        dp[n-1] = nums[n-1];
+        dp[n-2] = max(nums[n-1], nums[n-2]);
+
+        for(int i = n-3;i>=0;i--){
+            dp[i] = max(dp[i+1], nums[i] + dp[i+2]);
+        }
+        return dp[0];
+    }
+};
