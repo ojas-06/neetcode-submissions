4b6d48434df4191e313aa18d91bc21a2e437dfbd
Ojas Deshpande <ojas.md2006@gmail.com>
2026-06-23 17:24:27 +0530

    Add: climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/climbing-stairs/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+
+    int climbStairs(int n) {
+        if(n == 1) return 1;
+        if (n == 2) return 2;
+        vector<int> memo(n);
+        memo[0] = 1;
+        memo[1] = 2;
+        for(int i=2;i<n;i++) memo[i] = memo[i-1]+memo[i-2];
+        return memo[n-1];
+    }
+};
